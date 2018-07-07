3c43ba4835d2eaf21548c372499ca016e3d8abb5
kevllino <kevin.eid3@mail.dcu.ie>
2018-07-07 15:03:23 +0100

    Add lottery subproject

---
+++ solidity-udemy/lottery/deploy.js
@@ -0,0 +1,20 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
+const {interface, bytecode} = require("./compile");
+
+const provider = new HDWalletProvider(
+    'cook snap tiger poverty junior table what accuse cost where damp echo',
+    'https://rinkeby.infura.io/VrD0lE4lfWAwfSvG7mOz'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log("Attempting to deploy from account", accounts[0]);
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data: bytecode, arguments: []})
+        .send({ from: accounts[0], gas:1000000 })
+    console.log('Contract deployed to ', result.options.address)
+};
+deploy();
