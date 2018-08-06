8f344dab29991fb4b89a756b80da97ab8bce2366
kevllino <kevin.eid3@mail.dcu.ie>
2018-08-06 07:13:47 +0100

    Add ract sample project

---
+++ solidity-udemy/init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+npm install --save ganache-cli mocha solc fs-extra web3@1.0.0-beta.26 truffle-hdwallet-provider@0.0.3 
--- solidity-udemy/lottery/deploy.js
+++ solidity-udemy/lottery/deploy.js
@@ -15,6 +15,7 @@ const deploy = async () => {
     const result = await new web3.eth.Contract(JSON.parse(interface))
         .deploy({data: bytecode, arguments: []})
         .send({ from: accounts[0], gas:1000000 })
+    console.log(interface);
     console.log('Contract deployed to ', result.options.address)
 };
 deploy();
